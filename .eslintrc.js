c7239d0b6abd178a9cb83da38674cab0d0ca1903
scaler <zengkaiwang@yeah.net>
2021-12-11 12:52:47 +0800

    格式化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'semi': [2, 'never'],
+    'quotes': [2, 'single'],
   },
 };
